e0d912ea28c2e78160551cf40c5f80054fb5ec87
Yooshin Kim <yusinkim.or@gmail.com>
2024-03-12 17:17:01 +0900

    main 커밋

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <header className="App-header">변경변경</header>
+            <header className="App-header">Main 브랜치 </header>
         </div>
     );
 }
