19a2d9bbf4886322fc3f6029bab73081e385aae5
Yooshin Kim <yusinkim.or@gmail.com>
2024-03-12 21:34:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <header className="App-header">Test 브랜치 - 240312T2037 </header>
+            <header className="App-header">Test 브랜치 - 또 수정했지롱 🔥 </header>
         </div>
     );
 }
